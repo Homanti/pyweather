2f3d4f35c5d0e0b56c92c473a2f3c8a4b268fa97
Homanti <106923653+Homanti@users.noreply.github.com>
2023-04-26 19:13:13 +0200

    python

--- weather_main.py
+++ weather_main.py
@@ -6,12 +6,12 @@ from PyQt5.QtCore import Qt
 from datetime import datetime
 from PyQt5.QtGui import QBrush, QPixmap, QPalette
 
-
 #пусті зміні
 temperature = " "
 humidity = " "
 feelslike = " "
 description = " "
+change = "1"
 
 #словник
 words = {
@@ -26,6 +26,8 @@ words = {
     "labelTime": "🕘As of: ",
     "labelError": "⛔ Error, you probably entered the name of the city incorrectly.",
     "labelError2": "⛔ Error.",
+    "btnChange": "Change the theme of the interface",
+    "btnSettings": "Settings",
 
     "clear sky": "☀️ Clear sky",
     "few clouds": "🌤️ Few clouds",
@@ -70,6 +72,160 @@ words = {
     "squalls": "🌫️ Squalls",
     "tornado": "🌫️ Tornado",
 }
+#функція налаштування
+def openSettings():
+    #функція зміни мови
+    def localization():
+        global words
+        if words["btnLocalization"] == "Українська":
+            words = {
+                "lineWeather": "Напиши своє місто",
+                "btnLocalization": "English",
+                "btnSearch": "🔍 Пошук",
+                "labelTemp": "🌡️ Температура: ",
+                "labelFeelslike": "🌡️ Відчуваєтся як: ",
+                "labelHumidity": "💦 Вологість: ",
+                "labelCity": "🏙️ Місто: ",
+                "btnGps": "📍Пошук по геолокації",
+                "labelTime": "🕘Станом на: ",
+                "labelError": "⛔ Помилка, ймовірно, ви неправильно ввели назву міста.",
+                "labelError2": "⛔ Помилка.",
+                "btnChange": "Змінити тему інтерфейсу",
+                "btnSettings": "Налаштування",
+
+                "clear sky": "☀️ Чисте небо",
+                "few clouds": "🌤️ Небагато хмар",
+                "scattered clouds": "☁️ Хмарно",
+                "broken clouds": "☁️ Розірвані хмари",
+                "overcast clouds": "☁️ Похмурні хмари",
+
+                "light rain": "🌦️ Невеликий дощ",
+                "moderate rain": "🌦️ Помірний дощ",
+                "heavy intensity rain": "🌦️ Сильний дощ",
+                "very heavy rain": "🌦️ Дуже сильний дощ",
+                "extreme rain": "🌦️ Екстрімальний дощ",
+                "freezing rain": "🌦️ Крижаний дощ",
+                "light intensity shower rain": "🌦️ Дощ легкої інтенсивності",
+                "shower rain": "🌧️ Злива",
+                "heavy intensity shower rain": "🌦️ Дощ",
+                "ragged shower rain": "🌦️ Сильний зливовий дощ",
+                "rain": "🌦️ Дощ",
+                "thunderstorm": "⛈️ Гроза",
+
+                "light snow": "🌨️ Дрібний сніг",
+                "snow": "🌨️ Сніг",
+                "heavy snow": "🌨️ Сильний сніг",
+                "sleet": "🌨️ Мокрий сніг",
+                "light shower sleet": "🌨️ Сніг",
+                "shower sleet": "🌨️ Сніг",
+                "light rain and snow": "🌨️ Дрібний дощ зі снігом",
+                "rain and snow": "🌨️ Дощ і сніг",
+                "light shower snow": "🌨️ Невеликий сніг",
+                "shower snow": "🌨️ Снігова злива",
+                "heavy shower snow": "🌨️ Сильний сніг",
+
+                "mist": "🌫️ Туман",
+                "smoke": "🌫️ Дим",
+                "haze": "🌫️ Туман",
+                "sand/dust whirls": "🌫️ Кружляє пил",
+                "dust whirls": "🌫️ Кружляє пил",
+                "fog": "🌫️ Туман",
+                "sand": "🌫️ Пісок",
+                "dust": "🌫️ Кружляє пил",
+                "volcanic ash": "🌫️ Вулканічний попіл",
+                "squalls": "🌫️ Шквали",
+                "tornado": "🌫️ Торнадо",
+            }
+        else:
+            words = {
+                "lineWeather": "Write your city",
+                "btnLocalization": "Українська",
+                "btnSearch": "🔍 Search",
+                "labelTemp": "🌡️ Temperature: ",
+                "labelFeelslike": "🌡️ Feels like: ",
+                "labelHumidity": "💦 Humidity: ",
+                "labelCity": "🏙️ City: ",
+                "btnGps": "📍GPS",
+                "labelTime": "🕘As of: ",
+                "labelError": "⛔ Error, you probably entered the name of the city incorrectly.",
+                "labelError2": "⛔ Error.",
+                "btnChange": "Change the theme of the interface",
+                "btnSettings": "Settings",
+
+                "clear sky": "☀️ Clear sky",
+                "few clouds": "🌤️ Few clouds",
+                "scattered clouds": "☁️ Scattered clouds",
+                "broken clouds": "☁️ Broken clouds",
+                "overcast clouds": "☁️ Overcast clouds",
+
+                "light rain": "🌦️ Light rain",
+                "moderate rain": "🌦️ Moderate rain",
+                "heavy intensity rain": "🌦️ Heavy intensity rain",
+                "very heavy rain": "🌦️ Very heavy rain",
+                "extreme rain": "🌦️ Extreme rain",
+                "freezing rain": "🌦️ Freezing rain",
+                "light intensity shower rain": "🌦️ Light intensity shower rain",
+                "shower rain": "🌧️ Shower rain",
+                "heavy intensity shower rain": "🌦️ Heavy intensity shower rain",
+                "ragged shower rain": "🌦️ Ragged shower rain",
+                "rain": "🌦️ Rain",
+                "thunderstorm": "⛈️ Thunderstorm",
+
+                "light snow": "🌨️ Light snow",
+                "snow": "🌨️ Snow",
+                "heavy snow": "🌨️ Heavy snow",
+                "sleet": "🌨️ Sleet",
+                "light shower sleet": "🌨️ Light shower sleet",
+                "shower sleet": "🌨️ Shower sleet",
+                "light rain and snow": "🌨️ Light rain and snow",
+                "rain and snow": "🌨️ Rain and snow",
+                "light shower snow": "🌨️ Light shower snow",
+                "shower snow": "🌨️ Shower snow",
+                "heavy shower snow": "🌨️ Heavy shower snow",
+
+                "mist": "🌫️ Mist",
+                "smoke": "🌫️ Smoke",
+                "haze": "🌫️ Haze",
+                "sand/dust whirls": "🌫️ Dust",
+                "dust whirls": "🌫️ Dust",
+                "fog": "🌫️ Fog",
+                "sand": "🌫️ Sand",
+                "dust": "🌫️ Dust",
+                "volcanic ash": "🌫️ Ash",
+                "squalls": "🌫️ Squalls",
+                "tornado": "🌫️ Tornado",
+            }
+
+        lineWeather.setPlaceholderText(words["lineWeather"])
+        btnSearch.setText(words["btnSearch"])
+        btnGps.setText(words["btnGps"])
+        btnSettings.setText(words["btnSettings"])
+        btnLocalization.setText(words["btnLocalization"])
+        btnChange.setText(words["btnChange"])
+
+    dialog = QDialog()
+    dialog.setWindowTitle("Settings")
+    dialog.resize(300, 100)
+    layout = QVBoxLayout()
+
+    pixmap = QPixmap("data/background.png")
+    brush = QBrush(pixmap)
+    palette = QPalette()
+    palette.setBrush(QPalette.Background, brush)
+    dialog.setPalette(palette)
+
+    btnLocalization = QPushButton(words['btnLocalization'])
+    btnChange = QPushButton(words['btnChange'])
+
+    layout.addWidget(btnLocalization)
+    layout.addWidget(btnChange)
+
+    btnLocalization.clicked.connect(localization)
+    btnChange.clicked.connect(Change)
+
+    dialog.setLayout(layout)
+    dialog.exec_()
+
 
 #створення вікна
 app = QApplication([])
@@ -168,8 +324,7 @@ def gpssearch():
 #створення віджетів
 lineWeather = QLineEdit("")
 lineWeather.setPlaceholderText(words["lineWeather"])
-btnLocalization = QPushButton(words["btnLocalization"])
-btnLocalization.setObjectName("btnLocalization")
+btnSettings = QPushButton(words["btnSettings"])
 btnSearch = QPushButton(words["btnSearch"])
 btnGps = QPushButton(words["btnGps"])
 groupBackground = QGroupBox(" ")
@@ -207,138 +362,85 @@ mainLine.addWidget(groupBackground)
 horLine.addWidget(lineWeather)
 horLine.addWidget(btnSearch)
 horLine.addWidget(btnGps)
-horLine.addWidget(btnLocalization)
+horLine.addWidget(btnSettings)
 
 #розміщення головної лінії
 win.setLayout(mainLine)
 
-#функція локалізації
-def localization():
-    global words
-    if words["btnLocalization"] == "Українська":
-        words = {
-            "lineWeather": "Напиши своє місто",
-            "btnLocalization": "English",
-            "btnSearch": "🔍 Пошук",
-            "labelTemp": "🌡️ Температура: ",
-            "labelFeelslike": "🌡️ Відчуваєтся як: ",
-            "labelHumidity": "💦 Вологість: ",
-            "labelCity": "🏙️ Місто: ",
-            "btnGps": "📍Пошук по геолокації",
-            "labelTime": "🕘Станом на: ",
-            "labelError": "⛔ Помилка, ймовірно, ви неправильно ввели назву міста.",
-            "labelError2": "⛔ Помилка.",
+#функція зміни теми
+def Change():
+    global change
+    if change == "1":
+        change = "2"
+        app.setStyleSheet("""
+                QWidget {
+                    background: #25518e;
+                }
+                QLineEdit {
+                    background: #404040;
+                    border-radius: 5px;
+                    color: #B7B7B7;
+                    min-height: 50px;
+                    min-width: 100px;
+                    margin: 0px;
+                }
+                QPushButton {
+                    background: #1b6eb7;
+                    color: #B7B7B7;
+                    min-height: 50px;
+                    min-width: 110px;
+                    border-radius: 5px;
+                    margin: 0px;
+                }
+                QGroupBox {
+                    background: #242424;
+                    min-height: 100px;
+                    min-width: 600px;           
+                    border-radius: 5px;
+                    margin: 0px;
+                }
+                QLabel {
+                    color: #B7B7B7;
+                    font-size: 25px;
+                }
+                """)
 
-            "clear sky": "☀️ Чисте небо",
-            "few clouds": "🌤️ Небагато хмар",
-            "scattered clouds": "☁️ Хмарно",
-            "broken clouds": "☁️ Розірвані хмари",
-            "overcast clouds": "☁️ Похмурні хмари",
-
-            "light rain": "🌦️ Невеликий дощ",
-            "moderate rain": "🌦️ Помірний дощ",
-            "heavy intensity rain": "🌦️ Сильний дощ",
-            "very heavy rain": "🌦️ Дуже сильний дощ",
-            "extreme rain": "🌦️ Екстрімальний дощ",
-            "freezing rain": "🌦️ Крижаний дощ",
-            "light intensity shower rain": "🌦️ Дощ легкої інтенсивності",
-            "shower rain": "🌧️ Злива",
-            "heavy intensity shower rain": "🌦️ Дощ",
-            "ragged shower rain": "🌦️ Сильний зливовий дощ",
-            "rain": "🌦️ Дощ",
-            "thunderstorm": "⛈️ Гроза",
-
-            "light snow": "🌨️ Дрібний сніг",
-            "snow": "🌨️ Сніг",
-            "heavy snow": "🌨️ Сильний сніг",
-            "sleet": "🌨️ Мокрий сніг",
-            "light shower sleet": "🌨️ Сніг",
-            "shower sleet": "🌨️ Сніг",
-            "light rain and snow": "🌨️ Дрібний дощ зі снігом",
-            "rain and snow": "🌨️ Дощ і сніг",
-            "light shower snow": "🌨️ Невеликий сніг",
-            "shower snow": "🌨️ Снігова злива",
-            "heavy shower snow": "🌨️ Сильний сніг",
-
-            "mist": "🌫️ Туман",
-            "smoke": "🌫️ Дим",
-            "haze": "🌫️ Туман",
-            "sand/dust whirls": "🌫️ Кружляє пил",
-            "dust whirls": "🌫️ Кружляє пил",
-            "fog": "🌫️ Туман",
-            "sand": "🌫️ Пісок",
-            "dust": "🌫️ Кружляє пил",
-            "volcanic ash": "🌫️ Вулканічний попіл",
-            "squalls": "🌫️ Шквали",
-            "tornado": "🌫️ Торнадо",
-        }
     else:
-        words = {
-            "lineWeather": "Write your city",
-            "btnLocalization": "Українська",
-            "btnSearch": "🔍 Search",
-            "labelTemp": "🌡️ Temperature: ",
-            "labelFeelslike": "🌡️ Feels like: ",
-            "labelHumidity": "💦 Humidity: ",
-            "labelCity": "🏙️ City: ",
-            "btnGps": "📍GPS",
-            "labelTime": "🕘As of: ",
-            "labelError": "⛔ Error, you probably entered the name of the city incorrectly.",
-            "labelError2": "⛔ Error.",
-
-            "clear sky": "☀️ Clear sky",
-            "few clouds": "🌤️ Few clouds",
-            "scattered clouds": "☁️ Scattered clouds",
-            "broken clouds": "☁️ Broken clouds",
-            "overcast clouds": "☁️ Overcast clouds",
-
-            "light rain": "🌦️ Light rain",
-            "moderate rain": "🌦️ Moderate rain",
-            "heavy intensity rain": "🌦️ Heavy intensity rain",
-            "very heavy rain": "🌦️ Very heavy rain",
-            "extreme rain": "🌦️ Extreme rain",
-            "freezing rain": "🌦️ Freezing rain",
-            "light intensity shower rain": "🌦️ Light intensity shower rain",
-            "shower rain": "🌧️ Shower rain",
-            "heavy intensity shower rain": "🌦️ Heavy intensity shower rain",
-            "ragged shower rain": "🌦️ Ragged shower rain",
-            "rain": "🌦️ Rain",
-            "thunderstorm": "⛈️ Thunderstorm",
-
-            "light snow": "🌨️ Light snow",
-            "snow": "🌨️ Snow",
-            "heavy snow": "🌨️ Heavy snow",
-            "sleet": "🌨️ Sleet",
-            "light shower sleet": "🌨️ Light shower sleet",
-            "shower sleet": "🌨️ Shower sleet",
-            "light rain and snow": "🌨️ Light rain and snow",
-            "rain and snow": "🌨️ Rain and snow",
-            "light shower snow": "🌨️ Light shower snow",
-            "shower snow": "🌨️ Shower snow",
-            "heavy shower snow": "🌨️ Heavy shower snow",
-
-            "mist": "🌫️ Mist",
-            "smoke": "🌫️ Smoke",
-            "haze": "🌫️ Haze",
-            "sand/dust whirls": "🌫️ Dust",
-            "dust whirls": "🌫️ Dust",
-            "fog": "🌫️ Fog",
-            "sand": "🌫️ Sand",
-            "dust": "🌫️ Dust",
-            "volcanic ash": "🌫️ Ash",
-            "squalls": "🌫️ Squalls",
-            "tornado": "🌫️ Tornado",
-        }
-
-    lineWeather.setPlaceholderText(words["lineWeather"])
-    btnLocalization.setText(words["btnLocalization"])
-    btnSearch.setText(words["btnSearch"])
-    btnGps.setText(words["btnGps"])
+        change = "1"
+        app.setStyleSheet("""
+                QLineEdit {
+                    background: #404040;
+                    border-radius: 5px;
+                    color: #B7B7B7;
+                    min-height: 50px;
+                    min-width: 100px;
+                    margin: 0px;
+                }
+                QPushButton {
+                    background: #1b6eb7;
+                    color: #B7B7B7;
+                    min-height: 50px;
+                    min-width: 110px;
+                    border-radius: 5px;
+                    margin: 0px;
+                }
+                QGroupBox {
+                    background: #242424;
+                    min-height: 100px;
+                    min-width: 600px;           
+                    border-radius: 5px;
+                    margin: 0px;
+                }
+                QLabel {
+                    color: #555555;
+                    font-size: 25px;
+                }
+                """)
 
 #прив'язання кнопок до функцій
 btnSearch.clicked.connect(WeatherPrint)
-btnLocalization.clicked.connect(localization)
 btnGps.clicked.connect(gpssearch)
+btnSettings.clicked.connect(openSettings)
 
 #кінець)
 win.show()
